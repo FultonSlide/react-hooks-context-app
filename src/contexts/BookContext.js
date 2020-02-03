aee408c1efcf77ef283e320a3b086a4fcad316fc
William Qoro <Wi_llo@hotmail.com>
2020-02-03 17:38:09 +1100

    added components & context

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,13 +1,24 @@
 import React, { createContext, useState } from 'react'
 import uuid from 'uuid/v1'; //have to npm install this
-const BookContext = createContext();
+export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const { books, setBooks } = useState([
-        { title: 'name of the wind', author: 'patrick rothfuss', id: uuid() }
+    const [ books, setBooks ] = useState([
+        { title: 'name of the wind', author: 'patrick rothfuss', id: uuid() },
+        { title: 'the final empire', author: 'brandon sanderson', id: uuid() }
     ]); 
+
+    const addBook = (title, author) => {
+        setBooks([...books, {title, author}]);
+        console.log('added book');
+    };
+
+    const removeBook = (id) => {
+        setBooks(books.filter((book) => book.id !== id));
+    };
+
     return(
-        <BookContext.Provider value={{ books }}>
+        <BookContext.Provider value={{ books, addBook, removeBook }}>
             {props.children}
         </BookContext.Provider>
     );
