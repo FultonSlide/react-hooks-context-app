5ef0321cc7677025f1904b1f1765170efca54ad0
Will <Wi_llo@hotmail.com>
2020-02-04 14:03:01 +1100

    added local storage to context to persist data

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useState, useReducer } from 'react'
+import React, { createContext, useEffect, useReducer } from 'react'
 import { BookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [ books, dispatch ] = useReducer(BookReducer, []);
+    const [ books, dispatch ] = useReducer(BookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books]);
 
     return(
         <BookContext.Provider value={{ books, dispatch }}>
